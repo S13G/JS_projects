aea1213e8c08bf99db8e998341197c4fdbfdda07
s13g <ayflix0@gmail.com>
2023-01-15 19:53:33 +0100

    cc4.js

--- course/section2/script.js
+++ course/section2/script.js
@@ -210,7 +210,6 @@ console.log(jonas.getSummary())
 for (let rep = 1; rep <= 10; rep++) {
     console.log(`Lifting weights repetition ${rep}`)
 }
- */
 
 const jonas = [
     'Jonas',
@@ -222,6 +221,68 @@ const jonas = [
 
 const types = []
 
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i < jonas.length; i++) {
+    console.log(jonas[i], typeof jonas[i])
+
+    types.push(typeof jonas[i])
+}
+
+console.log(types)
+
+const years = [1991, 2007, 1969, 2020]
+const ages = []
+
+for (let i = 0; i < years.length; i++) {
+    ages.push(2037 - years[i])
+}
+
+console.log(ages)
+
+// continue and break
+
+for (let i = 0; i < jonas.length; i++) {
+    if (typeof jonas[i] !== 'string') continue;
     console.log(jonas[i], typeof jonas[i])
 }
+
+console.log('---BREAK WITH NUMBER---')
+for (let i = 0; i < jonas.length; i++) {
+    if (typeof jonas[i] === 'number') break;
+    console.log(jonas[i], typeof jonas[i])
+}
+
+const jonas = [
+    'Jonas',
+    'Sieg',
+    2037 - 1991,
+    'teacher',
+    ['Michael', 'Peter', 'Steven'],
+    true
+]
+
+for (let i = jonas.length - 1; i >= 0; i--) {
+    console.log(i, jonas[i])
+}
+
+for (let exercise = 1; exercise < 4; exercise++) {
+    console.log(`------- starting exercise ${exercise}`)
+    for (let rep = 1; rep < 6; rep++) {
+        console.log(`Exercise ${exercise}: Lifting weight repetition ${rep}`)
+    }
+}
+// let rep = 1
+// while (rep <= 10) {
+//     console.log(`Lifting weights repetition ${rep}`)
+//     rep++
+// }
+
+let dice = Math.trunc(Math.random() * 6) + 1
+
+while (dice !== 6) {
+    console.log(`You rolled a ${dice}`)
+    dice = Math.trunc(Math.random() * 6) + 1
+    if (dice === 6) {
+        console.log("Loop is about to end");
+    }
+}
+ */
