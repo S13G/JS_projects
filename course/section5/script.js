4b4659a07162d1eda3146b11cf6ef4a1114d384d
s13g <ayflix0@gmail.com>
2023-01-16 11:34:10 +0100

    challenge

--- course/section5/script.js
+++ course/section5/script.js
@@ -9,15 +9,22 @@ console.log(document.querySelector('.guess').value)
 // generate random number from 0 to 19
 const secretNumber = Math.trunc(Math.random() * 20) + 1
 let score = 20
-document.querySelector('.number').textContent = secretNumber
 
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value)
 
+    // when there is no input
     if (!guess) {
         document.querySelector('.message').textContent = 'No Number! 🔞'
+
+        // when player wins
     } else if (guess === secretNumber) {
+        document.querySelector('.number').textContent = secretNumber
         document.querySelector('.message').textContent = 'Correct Number! 🎉'
+        document.body.style.backgroundColor = '#60b347'
+        document.querySelector('.number').style.width = '30rem'
+
+        // when guess is too high
     } else if (guess > secretNumber) {
         document.querySelector('.message').textContent = 'Too High 📈'
         if (score > 1) {
@@ -27,6 +34,8 @@ document.querySelector('.check').addEventListener('click', function () {
             document.querySelector('.message').textContent = 'You lost the game 💥'
             document.querySelector('.score').textContent = 0
         }
+
+        // when guess is too low
     } else if (guess < secretNumber) {
         document.querySelector('.message').textContent = 'Too Low 📉'
         if (score > 1) {
@@ -37,4 +46,4 @@ document.querySelector('.check').addEventListener('click', function () {
             document.querySelector('.score').textContent = 0
         }
     }
-}) 
+})
